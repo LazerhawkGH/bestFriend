e943cc9d172075ee94b7945281bd8fe3d3c7d068
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-12-07 14:27:30 -0500

    Added scene

---
+++ bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLRoomOneController.java
@@ -0,0 +1,27 @@
+package asurza_maarse.bestfriend;
+
+/*
+ * Author: Zachary Maarse
+ * Date: Dec 7, 2018
+ * Purpose: 
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author zacharym44
+ */
+public class FXMLRoomOneController implements Initializable {
+
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
