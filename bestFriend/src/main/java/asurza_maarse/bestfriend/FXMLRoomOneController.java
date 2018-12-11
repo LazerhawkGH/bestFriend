2c33fdcaa431295971a6bd57514bec2d605128b5
user <user@acer>
2018-12-11 14:31:03 -0500

    working on parallax cuz Zach dumb

--- bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLRoomOneController.java
+++ bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLRoomOneController.java
@@ -5,12 +5,14 @@ package asurza_maarse.bestfriend;
  * Date: Dec 7, 2018
  * Purpose: Initial starting room
  */
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 
@@ -21,15 +23,27 @@ import javafx.scene.shape.Rectangle;
  */
 public class FXMLRoomOneController implements Initializable {
 
-   @FXML Pane pnParallax;
-   @FXML ImageView imgUser;
-   @FXML Rectangle recTop, recBottom, recLeft, recRight;
-   
-   
+    @FXML
+    Pane pnParallax;
     
+    @FXML
+    ImageView imgUser;
+    
+    @FXML
+    Rectangle recTop, recBottom, recLeft, recRight;
+    
+    @FXML
+    private void btnMove(KeyEvent evt){
+        if(evt.getCode() == KeyCode.ENTER){
+            pnParallax.setTranslateY(pnParallax.getTranslateY() + 20);
+        }      
+    }
+    
+    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
