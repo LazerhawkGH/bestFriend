c5f963c7e1700bbaffce7126158d881d4faf2505
user <user@acer>
2018-12-11 23:29:42 -0500

    Parallax and Collision works!

--- bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLRoomOneController.java
+++ bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLRoomOneController.java
@@ -14,6 +14,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 /**
@@ -24,22 +25,87 @@ import javafx.scene.shape.Rectangle;
 public class FXMLRoomOneController implements Initializable {
 
     @FXML
-    Pane pnParallax;
-    
+    private Pane pnParallax;
+
     @FXML
-    ImageView imgUser;
-    
+    private ImageView imgUser;
+
     @FXML
-    Rectangle recTop, recBottom, recLeft, recRight;
-    
+    private Circle cPlayer;
+
     @FXML
-    private void btnMove(KeyEvent evt){
-        if(evt.getCode() == KeyCode.ENTER){
-            pnParallax.setTranslateY(pnParallax.getTranslateY() + 20);
-        }      
+    private Rectangle wall1;
+
+    @FXML
+    private Rectangle recTop, recBottom, recLeft, recRight;
+
+    private Boolean up = false, down = false, left = false, right = false;
+
+    @FXML
+    private void btnMove(KeyEvent evt) {
+        if (collision(cPlayer, wall1)) {
+            if (up) {
+                pnParallax.setTranslateY(pnParallax.getTranslateY() - 5);
+                cPlayer.setTranslateY(cPlayer.getTranslateY() + 5);
+            } else if (down) {
+                pnParallax.setTranslateY(pnParallax.getTranslateY() + 5);
+                cPlayer.setTranslateY(cPlayer.getTranslateY() - 5);
+            } else if (left) {
+                pnParallax.setTranslateX(pnParallax.getTranslateX() - 5);
+                cPlayer.setTranslateX(cPlayer.getTranslateX() + 5);
+            } else if (right) {
+                pnParallax.setTranslateX(pnParallax.getTranslateX() + 5);
+                cPlayer.setTranslateX(cPlayer.getTranslateX() - 5);
+            }
+        } else {
+            if (evt.getCode() == KeyCode.UP) {
+                setdirFalse();
+                up = true;
+            } else if (evt.getCode() == KeyCode.DOWN) {
+                setdirFalse();
+                down = true;
+            } else if (evt.getCode() == KeyCode.LEFT) {
+                setdirFalse();
+                left = true;
+            } else if (evt.getCode() == KeyCode.RIGHT) {
+                setdirFalse();
+                right = true;
+            }
+            direction();
+        }
+
     }
-    
-    
+
+    private void setdirFalse() {
+        up = false;
+        down = false;
+        left = false;
+        right = false;
+    }
+
+    private void direction() {
+        if (up) {
+            pnParallax.setTranslateY(pnParallax.getTranslateY() + 5);
+            cPlayer.setTranslateY(cPlayer.getTranslateY() - 5);
+        } else if (down) {
+            pnParallax.setTranslateY(pnParallax.getTranslateY() - 5);
+            cPlayer.setTranslateY(cPlayer.getTranslateY() + 5);
+        } else if (left) {
+            pnParallax.setTranslateX(pnParallax.getTranslateX() + 5);
+            cPlayer.setTranslateX(cPlayer.getTranslateX() - 5);
+        } else if (right) {
+            pnParallax.setTranslateX(pnParallax.getTranslateX() - 5);
+            cPlayer.setTranslateX(cPlayer.getTranslateX() + 5);
+        } else {
+            setdirFalse();
+        }
+
+    }
+
+    public boolean collision(Circle block1, Rectangle block2) {
+//returns true if the areas intersect, false if they dont
+        return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
