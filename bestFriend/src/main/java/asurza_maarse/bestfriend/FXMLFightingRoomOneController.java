748593c62884bf99af0b44e6c7356e2eb9963580
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-12-12 14:31:00 -0500

    Added first fighting room, along with the FXML for it

---
+++ bestFriend/src/main/java/asurza_maarse/bestfriend/FXMLFightingRoomOneController.java
@@ -0,0 +1,29 @@
+package asurza_maarse.bestfriend;
+
+/*
+ * Author: Zachary Maarse
+ * Date: Dec 12, 2018
+ * Purpose: One of the rooms the player can encounter
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author zacharym44
+ */
+public class FXMLFightingRoomOneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
