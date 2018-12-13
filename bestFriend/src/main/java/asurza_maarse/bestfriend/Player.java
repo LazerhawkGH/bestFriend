0992fbcebaca346e805bf16c9a4a623da9279372
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-12-13 14:01:50 -0500

    Added the player class

---
+++ bestFriend/src/main/java/asurza_maarse/bestfriend/Player.java
@@ -0,0 +1,59 @@
+/*
+ * Author: Zachary Maarse
+ * Date: Dec 13, 2018
+ * Purpose: Player class
+ */
+package asurza_maarse.bestfriend;
+
+/**
+ *
+ * @author zacharym44
+ */
+public class Player {
+    
+    public String name;
+    public Boolean hasKnife;
+    public int health, atk;    
+    
+    public Player(){
+        name = "Elliot";
+        hasKnife= false;
+        health = 100;
+        atk = 5;
+    }
+    
+    public Player(String n, Boolean hk, int h, int a){
+        this.name = n;
+        this.hasKnife = hk;
+        this.health = h;
+        this.atk = a;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    
+    public Boolean getHasKnife(){
+        return hasKnife;
+    }
+        public void setHasKnife(Boolean hk){
+        this.hasKnife = hk;
+    }
+    
+    public int getAtk(){
+        return atk;
+    }
+    public void setAtk(int a){
+        this.atk = a;
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+    public void setHealth(int h){
+        this.health = h;
+    }
+}
